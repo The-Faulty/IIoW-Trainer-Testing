6709af88ff3478450ed6acf08bd29ee72edbf605
Andy Levine <andydandy4001@gmail.com>
2018-09-02 21:37:07 -0400

    Initial commit

---
+++ Trainer/Form1.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.button7 = new System.Windows.Forms.Button();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.Drag = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
+            this.dragControl1 = new WindowsFormsApp1.DragControl();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.Drag.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(9, 567);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connecting...";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label2.Location = new System.Drawing.Point(10, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label3.Location = new System.Drawing.Point(10, 550);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "v1.0.1";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.panel8);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.panel7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel6);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.panel5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 600);
+            this.panel1.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label4.Location = new System.Drawing.Point(52, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(121, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "By: The_Faulty";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.MidnightBlue;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label5.Location = new System.Drawing.Point(43, 3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 29);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "IIoW Trainer";
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel8.Location = new System.Drawing.Point(0, 441);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(16, 66);
+            this.panel8.TabIndex = 11;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.Control;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(13, 440);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(248, 69);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Settings";
+            this.button7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel7.Location = new System.Drawing.Point(0, 370);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(16, 66);
+            this.panel7.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.Control;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(13, 369);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(248, 69);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Shop";
+            this.button6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel6.Location = new System.Drawing.Point(0, 299);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(16, 66);
+            this.panel6.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.Control;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(13, 298);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(248, 69);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Weapon Hack";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel5.Location = new System.Drawing.Point(0, 228);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(16, 66);
+            this.panel5.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.Control;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(13, 227);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(248, 69);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Building Hack";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel4.Location = new System.Drawing.Point(0, 157);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(16, 66);
+            this.panel4.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(13, 156);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(248, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Block Hack";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Goldenrod;
+            this.panel3.Location = new System.Drawing.Point(0, 86);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(16, 66);
+            this.panel3.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MidnightBlue;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(13, 85);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(248, 69);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Money Cheat";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Drag
+            // 
+            this.Drag.BackColor = System.Drawing.Color.Goldenrod;
+            this.Drag.Controls.Add(this.button9);
+            this.Drag.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Drag.ForeColor = System.Drawing.Color.Maroon;
+            this.Drag.Location = new System.Drawing.Point(250, 0);
+            this.Drag.Name = "Drag";
+            this.Drag.Size = new System.Drawing.Size(750, 30);
+            this.Drag.TabIndex = 11;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Goldenrod;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.Control;
+            this.button9.Location = new System.Drawing.Point(720, 0);
+            this.button9.Margin = new System.Windows.Forms.Padding(0);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(30, 32);
+            this.button9.TabIndex = 0;
+            this.button9.Text = "X";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // dragControl1
+            // 
+            this.dragControl1.SelectControl = this.Drag;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(250, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(750, 570);
+            this.panel2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1000, 600);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.Drag);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "IIoW Money";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.Drag.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel Drag;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Button button5;
+        private DragControl dragControl1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Trainer/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Memory;
+using System.Net;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            backgroundWorker1.DoWork += new DoWorkEventHandler(backgroundWorker1_DoWork);
+            WebClient web = new WebClient();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        public Mem m = new Mem();
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Console.WriteLine("Started");
+            bool isOpened = false;
+            bool hasOpened = false;
+
+            while (true)
+            {
+
+                int procID = m.getProcIDFromName("IIslands of War");
+
+                double curM = 0;
+
+                if (procID > 0)
+                {
+                    m.OpenProcess(procID);
+
+                    isOpened = m.OpenProcess(procID);
+                    hasOpened = m.OpenProcess(procID);
+
+                }
+                else
+                {
+                    isOpened = false;
+                }
+
+                if (isOpened)
+                {
+                    label1.Invoke((MethodInvoker)delegate
+                    {
+                        label1.Text = "Connected";
+                    });
+                }
+                if (!isOpened && hasOpened)
+                {
+                    label1.Invoke((MethodInvoker)delegate
+                    {
+                        label1.Text = "Disconnected";
+                    });
+                }
+
+                Task.Delay(500);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Attempting to import money.");
+
+            string num = "";
+
+            m.writeMemory("IIslands Of War.exe+0049941C,0x60,0x10,0x118,0xA0", "double", num);
+            if (m.writeMemory("IIslands Of War.exe+0049941C,0x60,0x10,0x118,0xA0", "double", num))
+            {
+                label2.Text = ("Money Write Was Successful");
+                Console.WriteLine("Money successfully imported.");
+            }
+            else
+            {
+                label2.Text = ("Money Write Was Unsuccessful");
+                Console.WriteLine("Money was unable to be imported.");
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Attempting to import sand.");
+
+            string num = "";
+
+            m.writeMemory("IIslands Of War.exe+003E72A0,0x20,0x44,0x4,0x0", "double", num);
+            if (m.writeMemory("IIslands Of War.exe+003E72A0,0x20,0x44,0x4,0x0", "double", num))
+            {
+                label2.Text = ("Sand Import Was Successful");
+                Console.WriteLine("Sand successfully imported.");
+            }
+            else
+            {
+                label2.Text = ("Sand Import Was Unsuccessful");
+                Console.WriteLine("Sand was unable to be imported.");
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void panel9_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
